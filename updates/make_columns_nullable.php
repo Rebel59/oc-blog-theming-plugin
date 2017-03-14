a6a7f223432b929f49351ecfe8231c2dccbb5541
Antonie Hogewoning <anton@rebel59.nl>
2017-03-14 19:51:45 +0100

    Make columns nullable

---
+++ updates/make_columns_nullable.php
@@ -0,0 +1,16 @@
+<?php namespace Rebel59\Blogthemes\Updates;
+
+use Schema;
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+
+class MakeColumnsNullable extends Migration
+{
+    public function up()
+    {
+        Schema::table('rainlab_blog_categories', function(Blueprint $table) {
+            $table->string('color')->nullable()->change();
+            $table->longText('css')->nullable()->change();
+        });
+    }
+}
