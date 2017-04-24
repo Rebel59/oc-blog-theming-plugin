fa35c50930bfdde63193f79f9664db0b7ba68c37
Bastian Rang <justb81@users.noreply.github.com>
2017-04-24 13:41:06 +0200

    German translation

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,1 +1,20 @@
 <?php
+return [
+    'plugin' => [
+        'name' => 'Blog Kategoriethemen',
+        'description' => 'Bietet thematische Anpassungsmöglichkeiten für Kategorien des Rainlab Blog plugin.',
+    ],
+    'models' => [
+        'category' => [
+            'fields' => [
+                'color' => 'Kategoriefarbe',
+                'css' => 'Kategorie-CSS',
+                'theme_images' => 'Kategoriebild'
+            ]
+        ]
+    ],
+    'tabs' => [
+        'theme' => 'Thema',
+        'advanced_theme' => 'Erweitertes Thema'
+    ]
+];
